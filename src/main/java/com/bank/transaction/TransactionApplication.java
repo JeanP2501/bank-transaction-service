1c18e8525a148d32f032b8e3bcd485e305f89e56
JeanP2501 <jeanydrogos@gmail.com>
2025-12-02 16:03:23 -0500

    feat: eureka server implementation

--- src/main/java/com/bank/transaction/TransactionApplication.java
+++ src/main/java/com/bank/transaction/TransactionApplication.java
@@ -2,12 +2,14 @@ package com.bank.transaction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Main application class for Transaction Service
  * Manages all banking transactions
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TransactionApplication {
 
 	public static void main(String[] args) {
